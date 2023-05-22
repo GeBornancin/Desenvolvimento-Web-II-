8266b82df8f6b4ac43cd4e1b4fa3eb8820a375c2
Geovane Bornancin <95493767+GeBornancin@users.noreply.github.com>
2023-05-22 18:57:50 -0300

    Comitt inicial

---
+++ trabalho_bim/resources/views/eixos/index.blade.php
@@ -0,0 +1,13 @@
+@extends('templates.main')
+@section('titulo') Eixos @endsection
+@section('conteudo')
+
+    <div class="row">
+        <div class="col">
+            <x-datatableEixo crud="eixos" :header="['Nome', 'Ações']" :data="$data" :hide="[true, false]" />
+        </div>
+
+
+    </div>
+    
+    @endsection
