2368c9f4abf43c84b76518a637fb8e21e8cf2c8d
Geovane Bornancin <95493767+GeBornancin@users.noreply.github.com>
2023-06-01 21:13:04 -0300

    Test - Carga

---
+++ trabalho_bimestral/tests/Feature/DisciplinaTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Disciplina;
+use App\Models\Curso;
+use App\Models\Eixo;
+
+class DisciplinaTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testCarga(){
+        $eixo = new Eixo();
+        $eixo->nome = 'TECNOLOGIA INFORMAÇÃO';
+        $eixo->save();
+
+        $curso = new Curso();
+        $curso->nome = 'ANALISE E DESENVOLVIMENTO DE SISTEMAS';
+        $curso->sigla = 'TADS';
+        $curso->tempo = '3';
+        $curso->eixo_id = $eixo->id;
+        $curso->save();
+
+        //Carga com número médio (5000)
+        $disciplina = new Disciplina();
+        $disciplina->nome = 'Disciplina Teste';
+        $disciplina->curso_id = $curso->id;
+        $disciplina->carga = 1000; // número médio 
+        $disciplina->save();
+          
+        
+        $this->assertTrue($disciplina->carga >= 1 && $disciplina->carga <= 999);
+
+         
+        
+    }
+}
